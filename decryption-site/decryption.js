60b848fe987066be10d9c8f54e91e8adcb5cf431
IsabellaR-3008 <813iruiz@gmail.com>
2025-11-16 01:35:58 -0500

    html2 my side

--- decryption-site/decryption.js
+++ decryption-site/decryption.js
@@ -1,9 +1,7 @@
     function checkInput() {
-        //the value from the HTML input element
         let inputValue = document.getElementById("myInput").value;
         let outputElement = document.getElementById("outputMessage");
 
-        // if/else statement
         if (inputValue === "EMPOWERMENT") {
             outputElement.innerHTML = "You got it right!";
         } else if (inputValue === "IDK") {
@@ -12,3 +10,12 @@
             outputElement.innerHTML = "Try again!";
         }
     }
+
+
+        if (inputValue === "Inhale confidence, exhale doubt.") {
+            outputElement.innerHTML = "You got it correct. Amazing!";
+        } else if (inputValue === "IDK") {
+            outputElement.innerHTML = "You can do better!";
+        } else {
+            outputElement.innerHTML = "Try again!";
+        }
