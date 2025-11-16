959b2898a1489913a116450cc8da722946014078
IsabellaR-3008 <813iruiz@gmail.com>
2025-11-16 01:43:11 -0500

    need

--- decryption-site/decryption.js
+++ decryption-site/decryption.js
@@ -11,9 +11,10 @@
         }
     }
 
-    function checkInputLevel2() {
+    function checkInputLevel() {
         let inputValue = document.getElementById("myInput").value;
         let outputElement = document.getElementById("outputMessage");
+        
         if (inputValue === "inhale confidence exhale doubt") {
             outputElement.innerHTML = "You got it correct. Amazing!";
         } else if (inputValue === "IDK") {
