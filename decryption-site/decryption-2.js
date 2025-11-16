4a29e552ff4e0e1d2be5cee50fe6db5b04fd774e
AmbitiousS141 <stephanieu14136@gmail.com>
2025-11-16 03:20:23 -0500

    Finished Level 1 and 4!

---
+++ decryption-site/decryption-2.js
@@ -0,0 +1,10 @@
+function checkInput() {
+    let inputValue = document.getElementById("myInput").value;
+    let outputElement = document.getElementById("outputMessage");
+    
+    if (inputValue === "sisterhood") {
+            outputElement.innerHTML = "You got it correct. Amazing!";
+        } else {
+            outputElement.innerHTML = "Try again!"
+        }
+    }
