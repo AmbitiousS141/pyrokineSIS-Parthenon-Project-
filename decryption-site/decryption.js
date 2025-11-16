3a76eb3bf0f3a4c3cfd2540c8a8714100fc49545
IsabellaR-3008 <813iruiz@gmail.com>
2025-11-16 01:41:23 -0500

    Update decryption.js

--- decryption-site/decryption.js
+++ decryption-site/decryption.js
@@ -11,11 +11,12 @@
         }
     }
 
-
+    function checkInputLevel2() {
+        let inputValue = document.getElementById("myInput").value;
+        let outputElement = document.getElementById("outputMessage");
         if (inputValue === "inhale confidence exhale doubt") {
             outputElement.innerHTML = "You got it correct. Amazing!";
         } else if (inputValue === "IDK") {
-            outputElement.innerHTML = "You can do better!";
-        } else {
             outputElement.innerHTML = "Try again!";
         }
+    }
